c646d6b6d77ae8ac06a3eb8c37f867eb0207aa24
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2020-05-28 22:07:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static('public'));
 
 // set bodyparser:
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // set security:
 app.use(helmet());
